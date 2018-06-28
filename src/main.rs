e14c480117040040f8194f2f55ef66f6a85214ce
Sky Yin <sky.yin@gmail.com>
2018-06-28 14:14:04 +0800

    A freestanding rust binary

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![feature(panic_implementation)] // required for defining the panic handler
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_implementation]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    loop {}
+}
